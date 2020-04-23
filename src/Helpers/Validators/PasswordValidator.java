6b3e5d70d1566a2fff430356617612940775b314
Yordan-Petrov-git <y.petrov@ue-varna.bg>
2020-04-23 20:44:21 +0300

    Password hashing salting and validation added FIX

--- src/Helpers/Validators/PasswordValidator.java
+++ src/Helpers/Validators/PasswordValidator.java
@@ -2,28 +2,28 @@ package Helpers.Validators;
 
 public class PasswordValidator {
 
-    public static boolean validation_Password(final String PASSWORD_Arg) {
+    public static boolean validation_Password(final String passwordArg) {
         //Regular expression to validate password returns bool
         boolean result = false;
         try {
-            if (PASSWORD_Arg != null) {
+            if (passwordArg != null) {
                 //_________________________
                 //Parameteres
-                final String MIN_LENGHT = "8";
-                final String MAX_LENGHT = "64";
-                final boolean SPECIAL_CHAR_NEEDED = true;
+                final String passwordMinChars = "8";
+                final String passwordMaxChars = "64";
+                final boolean isSpecialCharacterNeeded = true;
 
                 //_________________________
                 //Modules
-                final String ONE_DIGIT = "(?=.*[0-9])";  //(?=.*[0-9]) a digit must occur at least once
-                final String LOWER_CASE = "(?=.*[a-z])";  //(?=.*[a-z]) a lower case letter must occur at least once
-                final String UPPER_CASE = "(?=.*[A-Z])";  //(?=.*[A-Z]) an upper case letter must occur at least once
-                final String NO_SPACE = "(?=\\S+$)";  //(?=\\S+$) no whitespace allowed in the entire string
-                //final String MIN_CHAR = ".{" + MIN_LENGHT + ",}";  //.{8,} at least 8 characters
-                final String MIN_MAX_CHAR = ".{" + MIN_LENGHT + "," + MAX_LENGHT + "}";  //.{5,10} represents minimum of 5 characters and maximum of 10 characters
+                final String oneDigit = "(?=.*[0-9])";  //(?=.*[0-9]) a digit must occur at least once
+                final String lowerCase = "(?=.*[a-z])";  //(?=.*[a-z]) a lower case letter must occur at least once
+                final String upperCase = "(?=.*[A-Z])";  //(?=.*[A-Z]) an upper case letter must occur at least once
+                final String noSpace = "(?=\\S+$)";  //(?=\\S+$) no whitespace allowed in the entire string
+                //final String minChar = ".{" + passwordMinChars + ",}";  //.{8,} at least 8 characters
+                final String minMaxChar = ".{" + passwordMinChars + "," + passwordMaxChars + "}";  //.{5,10} represents minimum of 8 characters and maximum of 64 characters
 
                 final String SPECIAL_CHAR;
-                if (SPECIAL_CHAR_NEEDED == true) {
+                if (isSpecialCharacterNeeded == true) {
                     SPECIAL_CHAR = "(?=.*[@#$%^&+=])"; //(?=.*[@#$%^&+=]) a special character must occur at least once
                 } else {
                     SPECIAL_CHAR = "";
@@ -31,9 +31,9 @@ public class PasswordValidator {
                 //_________________________
                 //Pattern
                 //String pattern = "(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&+=])(?=\\S+$).{8,}";
-                final String PATTERN = ONE_DIGIT + LOWER_CASE + UPPER_CASE + SPECIAL_CHAR + NO_SPACE + MIN_MAX_CHAR;
+                final String pattern = oneDigit + lowerCase + upperCase + SPECIAL_CHAR + noSpace + minMaxChar;
                 //_________________________
-                result = PASSWORD_Arg.matches(PATTERN);
+                result = passwordArg.matches(pattern);
                 //_________________________
             }
 
